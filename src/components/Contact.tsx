7c7d1f9f1004aec53eadd5d97037e8b3cb559846
Waheed <112928015+waheed3742@users.noreply.github.com>
2024-05-07 14:48:48 +0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,8 +12,7 @@ const Contact = () => {
           </div>
 
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-            Got a technical issue? Want to send feedback about a beta feature?
-            Need details about our Business plan? Let us know.
+            Feel free to reach out if you have any questions or need assistance with your video editing needs!
           </p>
           <div className="w-full">
             <form
